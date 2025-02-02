b575275efe8d8631b2e8db3712f4bdab80ede677
Michael Kokonu <62575805+mysomDeGeneral@users.noreply.github.com>
2025-02-02 18:51:14 +0000

    Add files via upload
    profile

---
+++ data.js
@@ -0,0 +1,48 @@
+const main = {
+  name: 'Michael Kokonu',
+  mail: 'fianyekukokonumichael@gmail.com',
+  img: 'https://drive.google.com/file/d/15HLYHrDCiSrzhRFVYOmyJMk4NpD9maWC/view?usp=drive_link',
+  role: ['Backend Developer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Home',
+      iconName: 'logo-firefox',
+      link: 'https://mysomDeGeneral.github.io'
+    },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/mysomDeGeneral'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/'
+    },
+    {
+      name: 'StackOverflow',
+      iconName: 'logo-stackoverflow',
+      link: '#'
+    },
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/FianyekuM'
+    },
+    {
+      name: 'npm',
+      iconName: 'logo-npm',
+      link: '#'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'yourmail@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: '#projects' },
+    { name: 'Blogs', link: '#blogs' }
+  ]
+};
